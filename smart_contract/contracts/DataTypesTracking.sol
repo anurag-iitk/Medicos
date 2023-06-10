512d64129fd32755e8dc75dcacf41e4204f3cf71
anurag-iitk <anurag@c3ihub.iitk.ac.in>
2023-06-10 23:23:26 +0530

    feat: update tracking type file

--- smart_contract/contracts/DataTypesTracking.sol
+++ smart_contract/contracts/DataTypesTracking.sol
@@ -1,39 +1,61 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-struct Producer {
-    uint producerId;
-    address producerAddress;
-    bytes32 registrationNumber;
-    string companyName;
-    string location;
-}
+library Types {
+    // Enum defining different user roles
+    enum UserRole {
+        Manufacturer, // 0
+        Supplier, // 1
+        Vendor, // 2
+        Customer // 3
+    }
 
-struct Retailer {
-    uint retailerId;
-    address retailerAddress;
-    bytes32 registrationNumber;
-    string retailerName;
-    string location;
-}
+    // Struct defining user details
+    struct UserDetails {
+        UserRole role; // User's role
+        address id_; // User's address
+        string name; // User's name
+        string email; // User's email
+    }
 
-struct Consumer {
-    uint consumerId;
-    bytes32 consumerAadhar;
-    string name;
-    string dob;
-    uint age;
-    string location;
-}
+    // Enum defining different product types
+    enum ProductType {
+        ANTIBIOTICS, // 0
+        VACCINES, // 1
+        BIOLOGICS, // 2
+        CONTROLLED_SUBSTANCE, // 3
+        MEDICAL_DEVICES // 4
+    }
 
-struct Medicine {
-    string name;
-    bytes32 batchNumber;
-    Producer producer;
-    Retailer retailer;
-    Consumer[] consumer;
-    uint maxQuantity;
-    uint quantitySold;
-    bool isSold;
-}
+    // Struct defining user's history
+    struct UserHistory {
+        address id_; // Account ID of the user
+        uint256 date; // Added or purchased date in epoch in UTC timezone
+    }
 
+    // Struct defining product's history
+    struct ProductHistory {
+        UserHistory manufacturer; // Manufacturer's history
+        UserHistory supplier; // Supplier's history
+        UserHistory vendor; // Vendor's history
+        UserHistory[] customers; // Customers' history (multiple customers)
+    }
+
+    // Struct defining a product
+    struct Product {
+        string name; // Product name
+        string manufacturerName; // Manufacturer's name
+        address manufacturer; // Manufacturer's address
+        uint256 manDateEpoch; // Manufacturing date in epoch
+        uint256 expDateEpoch; // Expiry date in epoch
+        bool isInBatch; // Flag indicating if the product is part of a batch
+        uint256 batchCount; // Quantity packed in a single batch
+        string barcodeId; // Product's barcode ID
+        string productImage; // URL or path to the product's image
+        ProductType productType; // Product type
+        string scientificName; // Scientific name of the product
+        string usage; // Usage instructions
+        string[] composition; // Array of product's composition
+        string[] sideEffects; // Array of known side effects
+    }
+}
